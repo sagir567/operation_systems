6fbe6e0d214c5fb57f310607dbcc275b66b74931
ubunto 22.0.4 <sagir567@gmail.com>
2023-04-22 20:18:57 +0200

    fix PartB.codecA

--- Task_2/Part_b/codecA.c
+++ Task_2/Part_b/codecA.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-void codecA(char *argv[], int flag)
-{
-    char* data =argv[2];
-    for (int i = 0; i < sizeof(argv); i++)
-    {
-        if (isupper(data[i]))
-            data[i] -= 32;
 
-        else if (islower(data[i]))
-            data[i] +=32;
-        else
-        {
+void codecA(char data[], int size) {
+
+
+    printf("data:%s\n",data);
+    for (int i = 0; i < size ;i++) {
+        if (isupper(data[i])) data[i] += 32;
+
+        else if (islower(data[i])) data[i] -= 32;
+
+        else {
             continue;
         }
     }
+
 }
 
 
--- Task_2/Part_b/encode.c
+++ Task_2/Part_b/encode.c
@@ -9,28 +9,32 @@ int main(int argc, char *argv[]){
     while(argc!=3){
         printf("encode/decode <codec> <message>");
     }
+    int size = strlen(argv[2]);
+    printf("size:%d\n",size);
+    char data[size];
+    strcpy(data,argv[2]);
     char *codec = argv[1];
     int flag =(strcmp(argv[0],"decode"))?0:1;
 
     if(strcmp(codec,"codecA")==0){
         void (*library);
-        void(*method)(char* [],int);
+        void(*method)(char[],int);
         library = dlopen("./codecA.so",RTLD_LAZY);
 
     
         if(!library){
-        perror("ERROR:dynamic library init faild\n");
+        perror("ERROR:dynamic library init failed\n");
         return 1;
         }
-        method = (void (*)(char*[], int))dlsym(library,"codecA");
+        method = (void (*)(char[], int))dlsym(library,"codecA");
     
         if(!method){
             perror("ERROR:method linker failure!\n");
             dlclose(library);
             return 1;
         }
-        method(argv,flag);
-        printf("%s",argv[2]);
+        method(data,size);
+        printf("%s\n",data);
         dlclose (library);
     }
 
